1da3a513bba2b726ef6e550e3742705d380a7dac
ZhiruXie <danielxie9735@gmail.com>
2025-11-24 21:14:14 +0800

    Creating a sample API and the first RESTful API

--- src/main/java/com/codewithmosh/store/controllers/HomeController.java
+++ src/main/java/com/codewithmosh/store/controllers/HomeController.java
@@ -1,15 +1,11 @@
 package com.codewithmosh.store.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-
     @RequestMapping("/")
     public String index(Model model){
         model.addAttribute("name","Daniel");
