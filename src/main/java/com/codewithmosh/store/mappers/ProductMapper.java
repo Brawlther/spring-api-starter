5f2c742bdf3e07de77c5faeb24f2159508b885f8
ZhiruXie <danielxie9735@gmail.com>
2025-11-29 22:07:10 +0800

    Automate product mapping

--- src/main/java/com/codewithmosh/store/mappers/ProductMapper.java
+++ src/main/java/com/codewithmosh/store/mappers/ProductMapper.java
@@ -3,12 +3,14 @@ package com.codewithmosh.store.mappers;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.codewithmosh.store.dtos.ProductDto;
 import com.codewithmosh.store.entities.Product;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
+    @Mapping(target = "categoryId", source = "category.id")
     ProductDto toDto(Product product);
     List<ProductDto> toDto(List<Product> products);
 }
