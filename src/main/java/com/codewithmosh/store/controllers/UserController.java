153b7afe117d726799ca3ed82c947e608daf70ed
ZhiruXie <danielxie9735@gmail.com>
2025-11-29 20:42:53 +0800

    Extracting Query Parameters

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -1,9 +1,13 @@
 package com.codewithmosh.store.controllers;
 
+import java.util.Set;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codewithmosh.store.dtos.UserDto;
@@ -20,8 +24,11 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping("")
-    public Iterable<UserDto> getAllUsers(){
-        return userRepository.findAll().stream().map(
+    public Iterable<UserDto> getAllUsers(@RequestParam(required = false, defaultValue = "", name="sort") String sortStr){
+        if(!Set.of("name","email").contains(sortStr)){
+            sortStr = "name";
+        }
+        return userRepository.findAll(Sort.by(sortStr)).stream().map(
             userMapper::toDto
         ).toList();
     }
