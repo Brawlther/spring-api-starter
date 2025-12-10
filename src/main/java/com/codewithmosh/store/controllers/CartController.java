3e4202ad26bba9ced33fe7a9e4848da33802d130
ZhiruXie <danielxie9735@gmail.com>
2025-12-10 11:09:14 +0800

    Implemented logic for adding a product to a cart.

--- src/main/java/com/codewithmosh/store/controllers/CartController.java
+++ src/main/java/com/codewithmosh/store/controllers/CartController.java
@@ -1,17 +1,29 @@
 package com.codewithmosh.store.controllers;
 
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import com.codewithmosh.store.dtos.AddItemToCartRequest;
 import com.codewithmosh.store.dtos.CartDto;
+import com.codewithmosh.store.dtos.CartItemDto;
 import com.codewithmosh.store.entities.Cart;
+import com.codewithmosh.store.entities.CartItem;
 import com.codewithmosh.store.mappers.CartMapper;
 import com.codewithmosh.store.repositories.CartRepository;
+import com.codewithmosh.store.repositories.ProductRepository;
 
 import lombok.AllArgsConstructor;
+import lombok.NonNull;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @AllArgsConstructor
 @RestController
@@ -19,6 +31,7 @@ import lombok.AllArgsConstructor;
 public class CartController {
     private final CartRepository cartRepository;
     private final CartMapper cartMapper;
+    private final ProductRepository productRepository;
 
     @PostMapping
     public ResponseEntity<CartDto> createCart(
@@ -30,5 +43,43 @@ public class CartController {
         var cartDto = cartMapper.toDto(cart);
         var uri = uriBuilder.path("/carts/{id}").buildAndExpand(cart.getId()).toUri();
         return ResponseEntity.created(uri).body(cartDto);
+    }
+
+
+    @PostMapping("/{cartId}/items")
+    public ResponseEntity<CartItemDto> addProductToCart(
+        @PathVariable @NonNull
+        UUID cartId,
+        @RequestBody
+        AddItemToCartRequest request
+    ) {
+        var cart = cartRepository.findById(cartId).orElse(null);
+        if(cart == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        var product =  productRepository.findById(request.getProductId()).orElse(null);
+        if(product == null){
+            return ResponseEntity.badRequest().build();
+        }
+
+        var cartItem = cart.getCartItems().stream()
+            .filter(item -> item.getProduct().getId().equals(product.getId()))
+            .findFirst()
+            .orElse(null);
+
+        if(cartItem != null){
+            cartItem.setQuantity(cartItem.getQuantity() + 1);
+        }
+        else{
+            cartItem = new CartItem();
+            cartItem.setProduct(product);
+            cartItem.setQuantity(1);
+            cartItem.setCart(cart);
+            cart.getCartItems().add(cartItem);
+        }
+        cartRepository.save(cart);
+        var cartItemDto = cartMapper.toDto(cartItem);
+        return ResponseEntity.status(HttpStatus.CREATED).body(cartItemDto);
     }
 }
--- src/main/java/com/codewithmosh/store/dtos/CartItemDto.java
+++ src/main/java/com/codewithmosh/store/dtos/CartItemDto.java
@@ -1,8 +1,12 @@
 package com.codewithmosh.store.dtos;
 
+import java.math.BigDecimal;
+
 import lombok.Data;
 
 @Data
 public class CartItemDto {
-    
+    CartProductDto product;
+    private int quantity;
+    private BigDecimal totalPrice;
 }
